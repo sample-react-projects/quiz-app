165e3b6e1d0988e9dce9c21a9c9ec3c7a7527852
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-24 23:11:07 +0530

    Add questions

---
+++ src/assets/questions.ts
@@ -0,0 +1,148 @@
+export const questions = [
+  {
+    id: "q1",
+    question: "Which of the following definitions best describes React.js?",
+    answers: [
+      {
+        id: "a1",
+        answer:
+          "A library to build user interfaces with help of declarative code.",
+      },
+      { id: "a2", answer: "A library for managing state in web applications." },
+      {
+        id: "a3",
+        answer:
+          "A framework to build user interfaces with help of imperative code.",
+      },
+      {
+        id: "a4",
+        answer: "A library used for building mobile applications only.",
+      },
+    ],
+    correctAnswer: "a1",
+  },
+  {
+    id: "q2",
+    question: "What purpose do React hooks serve?",
+    answers: [
+      {
+        id: "a1",
+        answer:
+          "Enabling the use of state and other React features in functional components.",
+      },
+      {
+        id: "a2",
+        answer: "Creating responsive layouts in React applications.",
+      },
+      { id: "a3", answer: "Handling errors within the application." },
+      {
+        id: "a4",
+        answer: "Part of the Redux library for managing global state.",
+      },
+    ],
+    correctAnswer: "a1",
+  },
+  {
+    id: "q3",
+    question: "Can you identify what JSX is?",
+    answers: [
+      {
+        id: "a1",
+        answer:
+          "A JavaScript extension that adds HTML-like syntax to JavaScript.",
+      },
+      {
+        id: "a2",
+        answer: "A JavaScript library for building dynamic user interfaces.",
+      },
+      {
+        id: "a3",
+        answer:
+          "A specific HTML version that was explicitly created for React.",
+      },
+      {
+        id: "a4",
+        answer: "A tool for making HTTP requests in a React application.",
+      },
+    ],
+    correctAnswer: "a1",
+  },
+  {
+    id: "q4",
+    question: "What is the most common way to create a component in React?",
+    answers: [
+      {
+        id: "a1",
+        answer:
+          "By defining a JavaScript function that returns a renderable value.",
+      },
+      { id: "a2", answer: "By defining a custom HTML tag in JavaScript." },
+      { id: "a3", answer: "By creating a file with a .jsx extension." },
+      {
+        id: "a4",
+        answer: 'By using the "new" keyword followed by the component name.',
+      },
+    ],
+    correctAnswer: "a1",
+  },
+  {
+    id: "q5",
+    question: 'What does the term "React state" imply?',
+    answers: [
+      {
+        id: "a1",
+        answer:
+          "An object in a component that holds values and may cause the component to render on change.",
+      },
+      { id: "a2", answer: "The lifecycle phase a React component is in." },
+      {
+        id: "a3",
+        answer:
+          "The overall status of a React application, including all props and components.",
+      },
+      {
+        id: "a4",
+        answer: "A library for managing global state in React applications.",
+      },
+    ],
+    correctAnswer: "a1",
+  },
+  {
+    id: "q6",
+    question: "How do you typically render list content in React apps?",
+    answers: [
+      {
+        id: "a1",
+        answer:
+          "By using the map() method to iterate over an array of data and returning JSX.",
+      },
+      {
+        id: "a2",
+        answer:
+          "By using the for() loop to iterate over an array of data and returning JSX.",
+      },
+      {
+        id: "a3",
+        answer:
+          "By using the forEach() method to iterate over an array of data and returning JSX.",
+      },
+      {
+        id: "a4",
+        answer:
+          "By using the loop() method to iterate over an array of data and returning JSX.",
+      },
+    ],
+    correctAnswer: "a1",
+  },
+  {
+    id: "q7",
+    question: "Which approach can NOT be used to render content conditionally?",
+    answers: [
+      { id: "a1", answer: "Using a the #if template syntax." },
+      { id: "a2", answer: "Using a ternary operator." },
+      { id: "a3", answer: "Using the && operator." },
+      { id: "a4", answer: "Using an if-else statement." },
+    ],
+    correctAnswer: "a1",
+  },
+];
