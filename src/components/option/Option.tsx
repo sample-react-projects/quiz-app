5233bad60d74e014a67c8e710366f59333fc2426
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-26 19:05:05 +0530

    Add  input to control selected item

--- src/components/option/Option.tsx
+++ src/components/option/Option.tsx
@@ -1,21 +1,46 @@
 interface IOption {
+  id: string;
   index: number;
   isCorrectAnswer: boolean;
   option: string;
+  questionId: string;
 }
+import { useState } from "react";
 import styles from "./Option.module.scss";
 
-const Option: React.FC<IOption> = ({ index, option, isCorrectAnswer }) => {
+const Option: React.FC<IOption> = ({
+  id,
+  index,
+  option,
+  isCorrectAnswer,
+  questionId,
+}) => {
   const prefix = String.fromCharCode(index + 97);
+  const [answered, setAnswered] = useState<boolean>(false);
+
+  function handleOptionChange() {
+    setAnswered(true);
+  }
 
   return (
-    <div
-      className={`${styles.option} ${
-        isCorrectAnswer ? styles.option__correct : null
-      }`}
-    >
-      {prefix}. {option}
-    </div>
+    <>
+      <input
+        type="radio"
+        className={styles.input}
+        disabled={answered}
+        name={questionId}
+        onChange={handleOptionChange}
+        id={id}
+      ></input>
+      <label
+        htmlFor={id}
+        className={`${styles.option} ${
+          isCorrectAnswer ? styles.option__correct : ""
+        }`}
+      >
+        {prefix}. {option}
+      </label>
+    </>
   );
 };
 
