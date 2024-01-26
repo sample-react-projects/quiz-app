31c0a91106002fc5a842ced1adb963b0ae2a6907
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-26 15:09:01 +0530

    Rename questions keys

--- src/assets/questions.ts
+++ src/assets/questions.ts
@@ -1,22 +1,24 @@
-export const questions = [
+import { Question } from "../models/Question";
+
+export const questions: Question[] = [
   {
     id: "q1",
     question: "Which of the following definitions best describes React.js?",
-    answers: [
+    options: [
       {
         id: "a1",
-        answer:
+        option:
           "A library to build user interfaces with help of declarative code.",
       },
-      { id: "a2", answer: "A library for managing state in web applications." },
+      { id: "a2", option: "A library for managing state in web applications." },
       {
         id: "a3",
-        answer:
+        option:
           "A framework to build user interfaces with help of imperative code.",
       },
       {
         id: "a4",
-        answer: "A library used for building mobile applications only.",
+        option: "A library used for building mobile applications only.",
       },
     ],
     correctAnswer: "a1",
@@ -24,20 +26,20 @@ export const questions = [
   {
     id: "q2",
     question: "What purpose do React hooks serve?",
-    answers: [
+    options: [
       {
         id: "a1",
-        answer:
+        option:
           "Enabling the use of state and other React features in functional components.",
       },
       {
         id: "a2",
-        answer: "Creating responsive layouts in React applications.",
+        option: "Creating responsive layouts in React applications.",
       },
-      { id: "a3", answer: "Handling errors within the application." },
+      { id: "a3", option: "Handling errors within the application." },
       {
         id: "a4",
-        answer: "Part of the Redux library for managing global state.",
+        option: "Part of the Redux library for managing global state.",
       },
     ],
     correctAnswer: "a1",
@@ -45,24 +47,24 @@ export const questions = [
   {
     id: "q3",
     question: "Can you identify what JSX is?",
-    answers: [
+    options: [
       {
         id: "a1",
-        answer:
+        option:
           "A JavaScript extension that adds HTML-like syntax to JavaScript.",
       },
       {
         id: "a2",
-        answer: "A JavaScript library for building dynamic user interfaces.",
+        option: "A JavaScript library for building dynamic user interfaces.",
       },
       {
         id: "a3",
-        answer:
+        option:
           "A specific HTML version that was explicitly created for React.",
       },
       {
         id: "a4",
-        answer: "A tool for making HTTP requests in a React application.",
+        option: "A tool for making HTTP requests in a React application.",
       },
     ],
     correctAnswer: "a1",
@@ -70,17 +72,17 @@ export const questions = [
   {
     id: "q4",
     question: "What is the most common way to create a component in React?",
-    answers: [
+    options: [
       {
         id: "a1",
-        answer:
+        option:
           "By defining a JavaScript function that returns a renderable value.",
       },
-      { id: "a2", answer: "By defining a custom HTML tag in JavaScript." },
-      { id: "a3", answer: "By creating a file with a .jsx extension." },
+      { id: "a2", option: "By defining a custom HTML tag in JavaScript." },
+      { id: "a3", option: "By creating a file with a .jsx extension." },
       {
         id: "a4",
-        answer: 'By using the "new" keyword followed by the component name.',
+        option: 'By using the "new" keyword followed by the component name.',
       },
     ],
     correctAnswer: "a1",
@@ -88,21 +90,21 @@ export const questions = [
   {
     id: "q5",
     question: 'What does the term "React state" imply?',
-    answers: [
+    options: [
       {
         id: "a1",
-        answer:
+        option:
           "An object in a component that holds values and may cause the component to render on change.",
       },
-      { id: "a2", answer: "The lifecycle phase a React component is in." },
+      { id: "a2", option: "The lifecycle phase a React component is in." },
       {
         id: "a3",
-        answer:
+        option:
           "The overall status of a React application, including all props and components.",
       },
       {
         id: "a4",
-        answer: "A library for managing global state in React applications.",
+        option: "A library for managing global state in React applications.",
       },
     ],
     correctAnswer: "a1",
@@ -110,25 +112,25 @@ export const questions = [
   {
     id: "q6",
     question: "How do you typically render list content in React apps?",
-    answers: [
+    options: [
       {
         id: "a1",
-        answer:
+        option:
           "By using the map() method to iterate over an array of data and returning JSX.",
       },
       {
         id: "a2",
-        answer:
+        option:
           "By using the for() loop to iterate over an array of data and returning JSX.",
       },
       {
         id: "a3",
-        answer:
+        option:
           "By using the forEach() method to iterate over an array of data and returning JSX.",
       },
       {
         id: "a4",
-        answer:
+        option:
           "By using the loop() method to iterate over an array of data and returning JSX.",
       },
     ],
@@ -137,11 +139,11 @@ export const questions = [
   {
     id: "q7",
     question: "Which approach can NOT be used to render content conditionally?",
-    answers: [
-      { id: "a1", answer: "Using a the #if template syntax." },
-      { id: "a2", answer: "Using a ternary operator." },
-      { id: "a3", answer: "Using the && operator." },
-      { id: "a4", answer: "Using an if-else statement." },
+    options: [
+      { id: "a1", option: "Using a the #if template syntax." },
+      { id: "a2", option: "Using a ternary operator." },
+      { id: "a3", option: "Using the && operator." },
+      { id: "a4", option: "Using an if-else statement." },
     ],
     correctAnswer: "a1",
   },
