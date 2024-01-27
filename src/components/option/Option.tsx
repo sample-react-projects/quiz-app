c982a1b207717d6dc7a8242dee65ced87c24c776
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-27 15:52:46 +0530

    cleanup option component

--- src/components/option/Option.tsx
+++ src/components/option/Option.tsx
@@ -1,45 +1,34 @@
 interface IOption {
-  id: string;
+  answered: boolean;
   index: number;
   isCorrectAnswer: boolean;
   option: string;
-  questionId: string;
 }
-import { useState } from "react";
 import styles from "./Option.module.scss";
 
 const Option: React.FC<IOption> = ({
-  id,
+  answered,
   index,
-  option,
   isCorrectAnswer,
-  questionId,
+  option,
 }) => {
   const prefix = String.fromCharCode(index + 97);
-  const [answered, setAnswered] = useState<boolean>(false);
 
-  function handleOptionChange() {
-    setAnswered(true);
+  function handleOptionClick() {
+    if (!answered) {
+    }
   }
 
   return (
     <>
-      <input
-        type="radio"
-        className={styles.input}
-        disabled={answered}
-        name={questionId}
-        onChange={handleOptionChange}
-        id={id}
-      ></input>
-      <label
-        htmlFor={id}
+      <div
+        onClick={handleOptionClick}
         className={`${styles.option} ${
           isCorrectAnswer ? styles.option__correct : ""
         }`}
       >
         {prefix}. {option}
-      </label>
+      </div>
     </>
   );
 };
