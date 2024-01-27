807f89dbf768f9a1f1679b1797b6ff360cffddef
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-27 19:09:26 +0530

    Component

--- src/components/quiz-result/QuizResult.tsx
+++ src/components/quiz-result/QuizResult.tsx
@@ -1,7 +1,21 @@
 import styles from "./QuizResult.module.scss";
 
-const QuizResult: React.FC<{}> = () => {
-  return <></>;
+interface IQuizResult {
+  correctAnswersCount: number;
+  totalQuestions: number;
+}
+const QuizResult: React.FC<IQuizResult> = ({
+  correctAnswersCount,
+  totalQuestions,
+}) => {
+  return (
+    <div className={styles.result}>
+      <h2>
+        You got {correctAnswersCount} out of {totalQuestions} correct!
+      </h2>
+      <button className={styles["result__restart"]}>Restart</button>
+    </div>
+  );
 };
 
 export default QuizResult;
