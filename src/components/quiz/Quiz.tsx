86273b41c2ff76ad038ed41f02b6d7086d6d75b8
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-27 18:51:27 +0530

    Use empty quiz result component

--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -2,6 +2,7 @@ import styles from "./Quiz.module.scss";
 import { questions } from "../../assets/questions";
 import QuestionRenderer from "../question/QuestionRenderer";
 import { useState } from "react";
+import QuizResult from "../quiz-result/QuizResult";
 
 const totalQuestions = questions.length;
 
@@ -21,16 +22,22 @@ function Quiz() {
         <h1 className={styles["quiz__header-title"]}>React Quiz</h1>
         <progress value={currentQuestionIndex} max={totalQuestions}></progress>
       </div>
-      <div className={styles.quiz__question}>
-        <QuestionRenderer
-          key={questions[currentQuestionIndex].id}
-          question={currentQuestion}
-          onAnswerSubmitted={handleAnswerSubmit}
-        ></QuestionRenderer>
-      </div>
-      <div className={styles.quiz__status}>
-        {currentQuestionIndex + 1} of {totalQuestions} Questions
-      </div>
+      {currentQuestionIndex < totalQuestions ? (
+        <>
+          <div className={styles.quiz__question}>
+            <QuestionRenderer
+              key={questions[currentQuestionIndex].id}
+              question={currentQuestion}
+              onAnswerSubmitted={handleAnswerSubmit}
+            ></QuestionRenderer>
+          </div>
+          <div className={styles.quiz__status}>
+            {currentQuestionIndex + 1} of {totalQuestions} Questions
+          </div>
+        </>
+      ) : (
+        <QuizResult></QuizResult>
+      )}
     </div>
   );
 }
