6e59cc85089fafdc480ebd4a58fcf60b75fd2f09
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-30 01:11:35 +0530

    use startQuiz state

--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -1,15 +1,30 @@
 import styles from "./Quiz.module.scss";
 import Card from "../ui/card/Card";
+import { useState } from "react";
+import Welcome from "../welcome/Welcome";
+import Questions from "../questions/Questions";
 
-function Quiz() {
+const Quiz: React.FC<{}> = ({}) => {
+  const [startQuiz, setStartQuiz] = useState<boolean>(false);
+
+  function handleStartQuiz() {
+    setStartQuiz(true);
+  }
+
   return (
     <div className={styles.quiz}>
       <div className={styles.quiz__header}>
         <h1 className={styles["quiz__header-title"]}>React Quiz</h1>
       </div>
-      <Card></Card>
+      <Card>
+        {startQuiz ? (
+          <Questions></Questions>
+        ) : (
+          <Welcome startQuiz={handleStartQuiz}></Welcome>
+        )}
+      </Card>
     </div>
   );
-}
+};
 
 export default Quiz;
